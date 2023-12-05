d1546afe183ea1ba1cf16d78cec41a972d01b239
jj-jinosko <jesscj@umich.edu>
2023-12-04 20:25:14 -0500

    fix reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,24 @@
-const reverseString = function() {
+// https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
 
+// const reverseString = function(word) {
+
+//     let str = "";
+//     for(let len = word.length - 1; len >= 0; len--){
+//         str += word[len];
+//     }
+//     return str;
+// };
+
+
+//alternate
+
+const reverseString = function(str) {
+
+let splitString = str.split("");
+let reverseArray = splitString.reverse();
+let newArray = reverseArray.join("");
+
+return newArray;
 };
 
 // Do not edit below this line
