5c14f8ded1c475b9da3da06c727d3cc2487f3a64
jj-jinosko <jesscj@umich.edu>
2023-12-04 20:01:50 -0500

    fix repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let output = string;
+    if(num < 0){
+        output = "ERROR";
+    }
+    else if (num == 0){
+        output = "";
+    }
+    else {
+        for (i = 0; i < num-1; i++){
+            output += string;
+        }
+    }
+return output;
 };
 
 // Do not edit below this line
