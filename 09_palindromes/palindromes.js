48593beba2ff1f6a93d74b99cc5423aa244468da
jj-jinosko <jesscj@umich.edu>
2024-01-10 16:28:15 -0500

    solved palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (entry) {
+    const entryClean = entry.toLowerCase().replace(/[^a-z0-9]/g, ""); // ^ means not the following characters
+    const reversed = entryClean.split("").reverse().join("");
+    if (entryClean == reversed){
+        return true;
+    }
+    return false;
 };
+
+// const palindromes = (entry) =>
 
 // Do not edit below this line
 module.exports = palindromes;
