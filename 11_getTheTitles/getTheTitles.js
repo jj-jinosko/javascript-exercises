b02c09f8c70bd837590cd134e0cbe334a5f9e735
jj-jinosko <jesscj@umich.edu>
2024-01-11 10:03:17 -0500

    add soln

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,14 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    return arr.map((obj)=>obj.title);
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+// array of objects
+// filter =array from t/f or map=new array
+
+// could flatten and just get book titles? no bc keys would be duplicated...?
+
+// look at each element
+// select book.title property?
