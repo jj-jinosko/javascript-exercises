8a8d4f142d195addf255f0ce518167f3a930078e
jj-jinosko <jesscj@umich.edu>
2024-01-10 23:26:25 -0500

    added solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    arr = [1, 1];
+    if(number<0){
+        return "OOPS";
+    }
+    for(let i=2; i<number; i++){
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[number-1];
 };
+
+// maybe there is a way to do this with a fn like replace?
 
 // Do not edit below this line
 module.exports = fibonacci;
+
